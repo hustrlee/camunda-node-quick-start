75182bd86f9b523f437c50fa4a20fbb3fdea00e7
Ryan Li <rlee@163.com>
2020-12-24 17:17:55 +0800

    ignore

--- event-test/index.js
+++ event-test/index.js
@@ -5,7 +5,24 @@ const bpmClient = axios.create({
 });
 
 const msgOpt = {
-  url: "",
+  url: "/message",
   method: "post",
-  data: {}
+  data: {
+    messageName: "msg_add_note",
+    processInstanceId: "ffe4390a-45c6-11eb-86f6-0242ac120002",
+    processVariables: {
+      someText: {
+        value: "第一条实例信息."
+      }
+    }
+  }
 };
+
+bpmClient
+  .request(msgOpt)
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
